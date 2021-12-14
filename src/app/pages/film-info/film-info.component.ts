2406fa12a5475495c921ac482eef840ec9b42109
terentyevaolga <terentyeevaolga@gmail.com>
2021-12-14 20:57:17 +0300

    added header

---
+++ src/app/pages/film-info/film-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-film-info',
+  templateUrl: './film-info.component.html',
+  styleUrls: ['./film-info.component.sass']
+})
+export class FilmInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
